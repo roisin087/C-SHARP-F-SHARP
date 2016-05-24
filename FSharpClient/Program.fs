3698e1547ca30e2672204fad9c32dc7f4fc17a05
Roisin O'Connor <reoconnor@ccs.local>
2016-05-24 08:40:54 +0100

    FSharp client consuming Asp.Net Api using JsonProvider from FSharp.Data library

---
+++ FSharpClient/Program.fs
@@ -0,0 +1,35 @@
+﻿#light
+open System.Net
+open System.IO
+open FSharp.Data
+
+
+let url = "http://localhost:60467//api/contact"
+
+let req = HttpWebRequest.Create(url)
+
+type Json = JsonProvider<"""http://localhost:60467//api/contact""">
+
+let resp = req.GetResponse()
+
+let contacts = Json.GetSamples()
+
+for contact in contacts do
+    printfn "#%d %s" contact.Id contact.Name
+
+System.Console.ReadLine()
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
